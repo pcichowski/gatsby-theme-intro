629b4f9abb42fb01e7531fe46e85b4401dcd8b45
Paweł Cichowski <cichowski.pawel1@gmail.com>
2023-12-23 16:10:55 +0100

    themes: gh-dark.js: add github dark theme
    Signed-off-by: Paweł Cichowski <cichowski.pawel1@gmail.com>

---
+++ gatsby-theme-intro/src/themes/gh-dark.js
@@ -0,0 +1,10 @@
+module.exports = {
+  'back': '#0d1117',
+  'front': '#fde9ef',
+  'lead': '#3770dd',
+  'lead-text': '#ffffff',
+  'line': '#fde9ef',
+  'skill-1': '#c21b5b',
+  'skill-2': '#3700b3',
+  'skill-3': '#03dac6',
+};
